cf641746e07740f51137440c3f1aeb5ad62c776f
Surendhar <surendhar@ymail.com>
2020-11-28 19:38:26 +0000

    Disable debug

--- opentamilweb/settings.py
+++ opentamilweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'USA'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not True
 
 ALLOWED_HOSTS = ['*']
 
