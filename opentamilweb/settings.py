be7851c16d70d546ab7055cc8b74df2016e99284
Muthu Annamalai <photonique@hey.com>
2024-01-01 22:35:31 -0800

    - fix Dockerfile - django postgres -fix

--- opentamilweb/settings.py
+++ opentamilweb/settings.py
@@ -79,14 +79,11 @@ WSGI_APPLICATION = "opentamilweb.wsgi.application"
 
 
 DATABASES = {
-    "default": os.environ.get('DATABASE_URL','postgres://postgres@db/postgres')
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": os.environ.get('DATABASE_URL','postgres://postgres@db/postgres')
+    }
 }
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#    }
-#}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
